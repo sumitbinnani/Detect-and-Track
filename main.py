592927faa38d1e57c2c5798fb329d76a0160289d
Sumit Binnani <sumit.binnani@gmail.com>
2018-11-28 21:56:50 -0800

    Updated main code to support CLI

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
+import argparse
 import logging
 import time
 
 from moviepy.editor import VideoFileClip
 
-from detection.mobilenet_ssd_detector import Detector
 from pipeline.pipeline import DetectAndTrack
 from tracking.kalman_tracker import KalmanTracker
 
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    detector = Detector()
+    parser = argparse.ArgumentParser(description='Detect and Track')
+    parser.add_argument("--input", dest="input_file",
+                        help="path for video file",
+                        default="video.mp4")
+    parser.add_argument("--output", dest="output_file",
+                        help="path for processed video file",
+                        default="output.mp4")
+    parser.add_argument("--detector", dest='detector',
+                        help="mobilenet or yolov3",
+                        default="yolov3", type=str)
+    parsed_args = parser.parse_args()
+
+    assert parsed_args.detector.lower() in ["mobilenet", "yolov3"]
+
+    if parsed_args.detector.lower() == "mobilenet":
+        from detection.mobilenet_ssd_detector import Detector
+        detector = Detector()
+    else:
+        from detection.yolo_v3_detector import Detector
+        detector = Detector()
+
     detect_and_track = DetectAndTrack(detector, KalmanTracker)
     start = time.time()
-    output = 'test_output.mp4'
-    clip1 = VideoFileClip("car25_compressed.mp4").subclip(180, 210)
+    clip1 = VideoFileClip(parsed_args.input_file)
     clip = clip1.fl_image(detect_and_track.pipeline)
-    clip.write_videofile(output, audio=False)
+    clip.write_videofile(parsed_args.output_file, audio=False)
     end = time.time()
     print(round(end - start, 2), 'Seconds to finish')
